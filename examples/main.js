f5ec682f6713f9a8138b1fdc4e01881bc886c15c
Just be free <justbefree@126.com>
2020-05-06 17:42:40 +0800

    remove meta keywords

--- examples/main.js
+++ examples/main.js
@@ -2,7 +2,8 @@
  * @Author: Just be free
  * @Date:   2020-01-20 17:15:01
  * @Last Modified by:   Just be free
- * @Last Modified time: 2020-04-29 11:09:00
+ * @Last Modified time: 2020-05-06 17:37:10
+ * @E-mail: justbefree@126.com
  */
 
 import Vue from "vue";
