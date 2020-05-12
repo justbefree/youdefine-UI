9940b06d8ad3499e5569e79ee54d9b81a640e407
Just be free <justbefree@126.com>
2020-05-12 16:45:49 +0800

    add full examples for iconfont

--- examples/yn-iconfont/iconfont.vue
+++ examples/yn-iconfont/iconfont.vue
@@ -1,17 +1,63 @@
 <template>
   <div>
     <h2>iconfont</h2>
-    <yn-iconfont name="iconcheckbox-checked" @click="handleClick"></yn-iconfont>
+    <yn-flex justifyContent="spaceAround" flexWrap="wrap" class="flex">
+      <yn-flex-item class="flex-item" v-for="(icon, key) in this.iconfontCollections" :key="key">
+        <yn-iconfont :name="icon" @click="handleClick"></yn-iconfont>
+        <span class="icon-name">{{icon}}</span>
+      </yn-flex-item>
+    </yn-flex>
   </div>
 </template>
 
 <script>
 export default {
   name: "YnIconfontPage",
+  data() {
+    return {
+      iconfontCollections: []
+    };
+  },
   methods: {
     handleClick(e) {
       console.log("111111", e);
+    },
+    loadAllIcons() {
+      window.addEventListener("DOMContentLoaded", () => {
+        const validIcons = [];
+        const svg = document.querySelector("svg");
+        if (!svg) {
+          console.log("Make sure the yn-iconfont component was installed.");
+          return false;
+        }
+        const symbol = svg.getElementsByTagName("symbol");
+        if (!symbol) {
+          console.log("Make sure the yn-iconfont component was installed.");
+          return false;
+        }
+        for (let key in symbol) {
+          if (symbol[key] && symbol[key].id) {
+            validIcons.push(symbol[key].id);
+          }
+        }
+        this.iconfontCollections = validIcons;
+      });
     }
+  },
+  mounted() {
+    this.loadAllIcons();
   }
 };
 </script>
+<style type="text/css" scoped="scoped">
+  .flex{
+    background: #eee;
+  }
+  .icon-name{
+    display: none;
+  }
+  .flex-item{
+    width: 50px;
+    margin: 10px;
+  }
+</style>
