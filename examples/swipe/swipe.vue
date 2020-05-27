ee1be7c0f54daed8fa287977e0ac6b120c30f051
Just be free <justbefree@126.com>
2020-05-27 18:48:07 +0800

    update swipe example

--- examples/swipe/swipe.vue
+++ examples/swipe/swipe.vue
@@ -20,9 +20,11 @@
         </yn-swipe-item>
       </yn-swipe>
       <div class="api">
-        <span>对外暴露的接口</span>
+        <span>对外接口</span>
         <yn-button @click="prev('swipe1')">prev</yn-button>
         <yn-button @click="next('swipe1')">next</yn-button>
+        <yn-button @click="open('swipe1')">open</yn-button>
+        <yn-button @click="open('swipe1')">close</yn-button>
       </div>
     </div>
     <div class="box">
@@ -42,9 +44,11 @@
         </yn-swipe-item>
       </yn-swipe>
       <div class="api">
-        <span>对外暴露的接口</span>
+        <span>对外接口</span>
         <yn-button @click="prev('swipe2')">prev</yn-button>
         <yn-button @click="next('swipe2')">next</yn-button>
+        <yn-button @click="next('swipe2')">open</yn-button>
+        <yn-button @click="next('swipe2')">close</yn-button>
       </div>
     </div>
   </div>
@@ -63,6 +67,12 @@ export default {
     },
     next(ref) {
       this.$refs[ref].next();
+    },
+    open(ref) {
+      this.$refs[ref].openImageViewer();
+    },
+    close(ref) {
+      this.$refs[ref].closeImageViewer();
     }
   }
 };
