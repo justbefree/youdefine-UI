f7e1ec639450444e446ad7c51fc1ea67f018a42e
Just be free <justbefree@126.com>
2020-05-29 12:15:13 +0800

    config build relative path

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,14 @@
 * @Author: Just be free
 * @Date:   2020-05-06 14:45:16
 * @Last Modified by:   Just be free
-* @Last Modified time: 2020-05-28 18:28:25
+* @Last Modified time: 2020-05-29 12:14:07
 * @E-mail: justbefree@126.com
 */
 process.env.VUE_APP_VERSION = require('./package.json').version;
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const publicPath = process.env.NODE_ENV === 'production' ? "youdefine-UI" : "/";
 module.exports = {
+  publicPath,
   configureWebpack: {
     devtool: "source-map",
     plugins: [
