23be2cc56f979591466ff88ac70fb0dc662b9c79
Just be free <justbefree@126.com>
2020-06-08 11:03:28 +0800

    Supported BEM methodology

--- src/modules/component/bem.js
+++ src/modules/component/bem.js
@@ -2,6 +2,56 @@
  * @Author: Just be free
  * @Date:   2020-06-04 17:52:38
  * @Last Modified by:   Just be free
- * @Last Modified time: 2020-06-04 17:52:38
+ * @Last Modified time: 2020-06-05 15:36:32
  * @E-mail: justbefree@126.com
  */
+
+// BLOCK
+/**
+Block names may consist of Latin letters, digits, and dashes. 
+To form a CSS class, add a short prefix for namespacing: .block
+**/
+
+// ELEMENT
+/**
+Element names may consist of Latin letters, digits, 
+dashes and underscores. CSS class is formed as block name 
+plus two underscores plus element name: .block__elem
+**/
+
+// MODIFIER
+/**
+Modifier names may consist of Latin letters, digits, 
+dashes and underscores. CSS class is formed as block’s or 
+element’s name plus two dashes: .block--mod or .block__elem--mod 
+and .block--color-black with .block--color-red. 
+Spaces in complicated modifiers are replaced by dash.
+**/
+
+import { isObject } from "../utils";
+const parse = (name) => {
+  if (isObject(name)) {
+    const key = Object.keys(name)[0];
+    const value = name[key];
+    if (isObject(value)) {
+      const attr = Object.keys(value)[0];
+      return `${key}--${attr}--${value[attr]}`;
+    } else {
+      return `${key}--${value}`;
+    }
+  } else {
+    return name;
+  }
+};
+export const createBem = (...args) => {
+  const [b = "", e = ""] = args;
+  const block = parse(b);
+  const element = parse(e);
+  return element === "" ? block : `${block}__${element}`;
+};
+
+// bem("block"); => .block {}
+// bem({ block: "green" }) => .block--green {}
+// bem("block", "input") => .block__input {}
+// bem("block", { input: "active" }) => block__input--active
+// bem({ block: "disabled" }, { input: "active" }) => block--disabled__input--active
--- src/modules/component/index.js
+++ src/modules/component/index.js
@@ -1,25 +1,29 @@
 /*
-* @Author: Just be free
-* @Date:   2020-05-13 18:08:08
-* @Last Modified by:   Just be free
-* @Last Modified time: 2020-05-27 19:47:20
-* @E-mail: justbefree@126.com
-*/
+ * @Author: Just be free
+ * @Date:   2020-05-13 18:08:08
+ * @Last Modified by:   Just be free
+ * @Last Modified time: 2020-06-05 14:08:35
+ * @E-mail: justbefree@126.com
+ */
 import { getConfig } from "./config";
+import { createBem } from "./bem";
 const prefix = getConfig("VUE_APP_PREFIX");
 console.log("组件库前缀是", prefix);
 import { capitalize } from "../utils";
 export const defineComponent = (options = {}) => {
   options.name = genComponentName(options.name);
-  let data = typeof options.data === "function" && options.data() || {};
+  let data = (typeof options.data === "function" && options.data()) || {};
   if (options.components) {
     options.components = dependanceComponent(options.components);
   }
   options.data = function () {
-    const propsToData = options.initPropsToData && typeof options.initPropsToData === "function" && options.initPropsToData();
+    const propsToData =
+      options.initPropsToData &&
+      typeof options.initPropsToData === "function" &&
+      options.initPropsToData();
     const result = { ...data, VUE_APP_PREFIX: prefix };
     if (Array.isArray(propsToData)) {
-      propsToData.forEach(prop => {
+      propsToData.forEach((prop) => {
         const { key, value, parse } = prop;
         if (parse && typeof parse === "function") {
           result[key] = parse(this[value]);
@@ -29,18 +33,18 @@ export const defineComponent = (options = {}) => {
       });
     }
     return result;
-  }
+  };
   return options;
 };
 export const install = (component) => {
   component.install = (Vue) => {
     Vue.component(component.name, component);
-  }
+  };
   return component;
 };
 const dependanceComponent = (dependance = {}) => {
   const components = {};
-  Object.keys(dependance).forEach(key => {
+  Object.keys(dependance).forEach((key) => {
     components[genComponentName(key)] = dependance[key];
   });
   return components;
@@ -51,4 +55,7 @@ export const genComponentName = (name) => {
   } else {
     return capitalize(name);
   }
+};
+export const bem = (...args) => {
+  return createBem(...args);
 };
