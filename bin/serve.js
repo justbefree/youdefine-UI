40b354df0fa89c3010150a7e85201818cada1cc1
Just be free <justbefree@126.com>
2020-06-08 18:31:12 +0800

    Supported windows run dev serve

--- bin/serve.js
+++ bin/serve.js
@@ -2,23 +2,29 @@
 * @Author: Just be free
 * @Date:   2020-05-26 16:09:34
 * @Last Modified by:   Just be free
-* @Last Modified time: 2020-05-27 15:19:24
+* @Last Modified time: 2020-06-08 18:29:22
 * @E-mail: justbefree@126.com
 */
 const path = require("path");
+const os=require('os');
 const fs = require("fs");
-const serve = require("../build/serve");
+const serve = require("../build/serve/index.js");
 const previousPrefix = serve.getPreviousPrefix(path.join(__dirname, "../examples/layout/layout.vue"));
-const { execFile } = require('child_process');
-// console.log("之前的前缀是什么", previousPrefix);
-fs.chmod('build/serve.sh', 0o775, (err) => {
-  if (err) throw err;
-  console.log('The permissions for file "build/serve.sh" have been changed!');
-});
+const servejs = require("../build/serve.js")();
+console.log("之前的前缀是什么", previousPrefix);
+if (os.platform() === "darwin") {
+  const { execFile } = require('child_process');
+  fs.chmod('build/serve.sh', 0o775, (err) => {
+    if (err) throw err;
+    console.log('The permissions for file "build/serve.sh" have been changed!');
+  });
+  const child = execFile('build/serve.sh', ["-P", previousPrefix], { shell: true }, (error, stdout, stderr) => {
+    if (error) {
+      throw error;
+    }
+    console.log(stdout);
+  });
+} else {
+  servejs.run({ previousPrefix });
+}
 
-const child = execFile('build/serve.sh', ["-P", previousPrefix], { shell: true }, (error, stdout, stderr) => {
-  if (error) {
-    throw error;
-  }
-  console.log(stdout);
-});
