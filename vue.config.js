3b7fe3c12450a45f5cc5030c0abc54c79c3762a5
Just be free <justbefree@126.com>
2020-06-15 19:13:05 +0800

    Do not extract static file like svg /img

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,14 @@
 * @Author: Just be free
 * @Date:   2020-05-06 14:45:16
 * @Last Modified by:   Just be free
-* @Last Modified time: 2020-05-19 18:13:11
+* @Last Modified time: 2020-06-15 19:11:12
 * @E-mail: justbefree@126.com
 */
 process.env.VUE_APP_VERSION = require('./package.json').version;
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
+  publicPath: "./",
+  assetsDir: "static",
   configureWebpack: {
     devtool: "source-map",
     plugins: [
@@ -22,19 +24,18 @@ module.exports = {
   css: {
     extract: false
   },
-  // chainWebpack: config => {
-  //   config.module
-  //     .rule('js')
-  //     .include
-  //     .add('/src/components/index.js')
-  //     .end()
-  //     .use('babel')
-  //     .loader('babel-loader')
-  //     .tap(options => {
-  //       // 修改它的选项...
-  //       return options
-  //     })
-  // },
+  chainWebpack: config => {
+    const svgRule = config.module.rule('svg');
+    // 清除已有的所有 loader。
+    // 如果你不这样做，接下来的 loader 会附加在该规则现有的 loader 之后。
+    svgRule.uses.clear();
+    config.module
+      .rule('images')
+      .test(/\.(png|jpe?g|gif|webp|svg)(\?.*)?$/)
+        .use('url-loader')
+          .loader('url-loader')
+          .tap(options => Object.assign(options, { limit: 10240 }))
+  },
   pages: {
     examples: {
       entry: "examples/main.js",
