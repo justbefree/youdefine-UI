00bea6f8f38b5a135dfc08e32ef39f38a3fb887a
Just be free <justbefree@126.com>
2020-09-11 12:02:58 +0800

    Update iconfont examples

--- examples/iconfont/iconfont.vue
+++ examples/iconfont/iconfont.vue
@@ -1,12 +1,8 @@
 <template>
   <div>
     <h2>iconfont</h2>
-    <yn-flex justifyContent="spaceAround" flexWrap="wrap" class="flex">
-      <yn-flex-item class="flex-item" v-for="(icon, key) in this.iconfontCollections" :key="key">
-        <yn-iconfont :name="icon" @click="handleClick"></yn-iconfont>
-        <span class="icon-name">{{icon}}</span>
-      </yn-flex-item>
-    </yn-flex>
+    <yn-iconfont name="elm" size="40" @click="handleClick"></yn-iconfont>
+    <yn-iconfont name="eldddm"></yn-iconfont>
   </div>
 </template>
 
@@ -22,38 +18,7 @@ export default {
   methods: {
     handleClick(e) {
       console.log("111111", e);
-    },
-    loadAllIcons() {
-      if (this.loaded) {
-        return;
-      }
-      const validIcons = [];
-      const svg = document.querySelector("svg");
-      if (!svg) {
-        console.log(1, "Make sure the yn-iconfont component was installed.");
-        return false;
-      }
-      const symbol = svg.getElementsByTagName("symbol");
-      if (!symbol) {
-        console.log(2, "Make sure the yn-iconfont component was installed.");
-        return false;
-      }
-      for (let key in symbol) {
-        if (symbol[key] && symbol[key].id) {
-          validIcons.push(symbol[key].id);
-        }
-      }
-      this.iconfontCollections = validIcons;
-      this.loaded = true;
     }
-  },
-  mounted() {
-    this.$nextTick(() => {
-      this.loadAllIcons();
-    });
-    window.addEventListener("DOMContentLoaded", () => {
-      this.loadAllIcons();
-    });
   }
 };
 </script>
