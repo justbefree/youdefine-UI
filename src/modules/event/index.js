d7056d57c5cce4d4c9d1e0fa8901195de728d8f4
Just be free <justbefree@126.com>
2020-11-03 15:29:02 +0800

    Fix: unbindevent isn't work

--- src/modules/event/index.js
+++ src/modules/event/index.js
@@ -2,21 +2,22 @@
  * @Author: Just be free
  * @Date:   2020-03-31 12:10:32
  * @Last Modified by:   Just be free
- * @Last Modified time: 2020-05-06 16:10:30
+ * @Last Modified time: 2020-11-03 15:28:05
  * @E-mail: justbefree@126.com
  */
- /* eslint-disable */ 
+/* eslint-disable */
+
 let passiveSupported = false;
 try {
   let options = Object.defineProperty({}, "passive", {
-    get: function() {
+    get: function () {
       passiveSupported = true;
-    }
+    },
   });
   window.addEventListener("test", null, options);
 } catch (err) {}
 export const bind = (dom, type, fn, args, passive = true) => {
-  const callback = e => {
+  const callback = (e) => {
     fn.call(this, e, args);
   };
   dom.addEventListener(
@@ -25,7 +26,7 @@ export const bind = (dom, type, fn, args, passive = true) => {
     passiveSupported ? { passive, capture: false } : false
   );
   return () => {
-    dom.removeEventListener(type, callback);
+    off(dom, type, callback);
   };
 };
 // https://developer.mozilla.org/zh-CN/docs/Web/API/EventTarget/addEventListener
@@ -38,10 +39,10 @@ export const on = (dom, type, fn, passive = true) => {
 };
 
 export const off = (dom, type, fn) => {
-  dom.addEventListener(type, fn);
+  dom.removeEventListener(type, fn);
 };
 
-export const stopPropagation = event => {
+export const stopPropagation = (event) => {
   event &&
     typeof event.stopPropagation === "function" &&
     event.stopPropagation();
