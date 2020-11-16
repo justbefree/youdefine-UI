88f4c24c66865236a4e66b3d7917a16780eb0a70
Just be free <justbefree@126.com>
2020-11-16 17:46:48 +0800

    Eventbus

---
+++ src/modules/event/bus.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: Just be free
+ * @Date:   2020-11-16 14:34:03
+ * @Last Modified by:   Just be free
+ * @Last Modified time: 2020-11-16 14:34:43
+ * @E-mail: justbefree@126.com
+ */
+
+import Vue from "vue";
+export const EventBus = new Vue();
