9c0b653b090d810e7fc60463bfb205ac22b26e05
Just be free <justbefree@126.com>
2020-12-17 17:06:14 +0800

    Update types

--- types/dropdown-menu-item.d.ts
+++ types/dropdown-menu-item.d.ts
@@ -2,7 +2,7 @@
 * @Author: Just be free
 * @Date:   2020-12-17 16:41:13
 * @Last Modified by:   Just be free
-* @Last Modified time: 2020-12-17 17:03:24
+* @Last Modified time: 2020-12-17 17:05:44
 * @E-mail: justbefree@126.com
 */
 
@@ -13,5 +13,5 @@ export interface ToggleOptions {
 export class YnDropdownMenuItem extends YnComponents {
   closeTab(): void;
   toggle(args: ToggleOptions): void;
-  check(status: boolean, options: ToggleOptions): void;
+  check(status: boolean, options?: ToggleOptions): void;
 }
