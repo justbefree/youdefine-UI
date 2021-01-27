402395f8ab7ad6ce7def5b1cb7814571d5802f85
Just be free <justbefree@126.com>
2021-01-27 13:38:49 +0800

    Emit animation event hooks

--- src/dropdown/dropdown.js
+++ src/dropdown/dropdown.js
@@ -2,7 +2,7 @@
  * @Author: Just be free
  * @Date:   2021-01-26 15:32:36
  * @Last Modified by:   Just be free
- * @Last Modified time: 2021-01-27 12:57:31
+ * @Last Modified time: 2021-01-27 13:37:16
  * @E-mail: justbefree@126.com
  */
 import { defineComponent, genComponentName } from "../modules/component";
@@ -12,24 +12,86 @@ export default defineComponent({
   name: "Dropdown",
   mixins: [slotsMixins],
   components: { Popup },
+  props: {
+    direction: String,
+  },
   data() {
     return {
       show: false,
+      status: true,
     };
+  },
+  computed: {
+    position() {
+      return this.direction === "up" ? "bottom" : "top";
+    },
   },
   methods: {
     toggle() {
       console.log("ddd");
+      this.show = !this.show;
+    },
+    handleBeforeEnter() {
+      this.$emit("beforeEnter");
+      this.status = true;
+      const target = this.slots("target");
+      const dom = target[0].elm;
+      const rect = dom.getBoundingClientRect();
+      if (this.direction === "up") {
+        this.$refs.dropdownPopup.style.bottom = `${
+          window.innerHeight - rect.top
+        }px`;
+      } else {
+        this.$refs.dropdownPopup.style.top = `${rect.bottom}px`;
+      }
+      console.log("beforeEnter", target[0].elm);
+    },
+    handleAfterEnter() {
+      this.$emit("afterEnter");
+    },
+    handleBeforeLeave() {
+      this.$emit("beforeLeave");
+    },
+    handleAfterLeave() {
+      this.$emit("afterLeave");
+      this.status = false;
     },
   },
   render(h) {
     const target = this.slots("target");
-    console.log("target", target);
     return h("div", { class: ["yn-dropdown"] }, [
       target,
-      h(genComponentName("popup"), { props: { value: this.show } }, [
-        this.slots("content"),
-      ]),
+      h(
+        "div",
+        {
+          directives: [{ name: "show", value: this.status }],
+          class: [
+            "yn-dropdown-popup",
+            this.direction === "up" ? "top" : "bottom",
+          ],
+          ref: "dropdownPopup",
+        },
+        [
+          h(
+            genComponentName("popup"),
+            {
+              on: {
+                beforeEnter: this.handleBeforeEnter,
+                afterEnter: this.handleAfterEnter,
+                beforeLeave: this.handleBeforeLeave,
+                afterLeave: this.handleAfterLeave,
+              },
+              style: { position: "absolute" },
+              props: {
+                value: this.show,
+                singleton: true,
+                position: this.position,
+              },
+            },
+            [this.slots("content")]
+          ),
+        ]
+      ),
     ]);
   },
 });
