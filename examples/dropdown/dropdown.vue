75f554f8b8257a24312dccef18b3a421ee9ac41b
Just be free <justbefree@126.com>
2021-01-28 12:16:11 +0800

    Update examples

--- examples/dropdown/dropdown.vue
+++ examples/dropdown/dropdown.vue
@@ -5,9 +5,16 @@
       <yn-button slot="target" @click="toggle">以下</yn-button>
       <div slot="content">里面是内容</div>
     </yn-dropdown>
-
     <yn-dropdown ref="dropup" direction="up">
       <yn-button slot="target" @click="toggle2">以上</yn-button>
+      <div slot="content">里面是内容</div>
+    </yn-dropdown>
+    <yn-dropdown ref="left" direction="left">
+      <yn-button slot="target" class="left" @click="toggle3">左边</yn-button>
+      <div slot="content">里面是内容</div>
+    </yn-dropdown>
+    <yn-dropdown ref="right" direction="right">
+      <yn-button slot="target" @click="toggle4">右边</yn-button>
       <div slot="content">里面是内容</div>
     </yn-dropdown>
   </div>
@@ -26,12 +33,19 @@ export default {
     },
     toggle2() {
       this.$refs.dropup.toggle();
+    },
+    toggle3() {
+      this.$refs.left.toggle();
+    },
+    toggle4() {
+      this.$refs.right.toggle();
     }
   }
 };
 </script>
 <style type="text/css" scoped>
-.box {
-  height: 300px;
+.left {
+  margin: 0 auto;
+  display: block;
 }
 </style>
