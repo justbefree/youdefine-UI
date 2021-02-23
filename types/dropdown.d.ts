dc3c7c2b8731b19e491363d7cfe887221443fd72
Just be free <justbefree@126.com>
2021-02-23 15:03:25 +0800

    Add d.ts file

---
+++ types/dropdown.d.ts
@@ -0,0 +1,10 @@
+/*
+* @Author: Just be free
+* @Date:   2021-02-23 15:01:11
+* @Last Modified by:   Just be free
+* @Last Modified time: 2021-02-23 15:01:43
+* @E-mail: justbefree@126.com
+*/
+import { YnComponents } from "./component";
+export class YnDropdown extends YnComponents {
+}
