b6c98765c34c9f3763582c501a9e0aeca14968dd
Just be free <justbefree@126.com>
2021-08-16 15:08:42 +0800

    feat: support parent node click to control model

--- src/checkbox/checkbox.js
+++ src/checkbox/checkbox.js
@@ -2,7 +2,7 @@
  * @Author: Just be free
  * @Date:   2020-01-16 12:49:59
  * @Last Modified by:   Just be free
- * @Last Modified time: 2021-01-14 13:45:54
+ * @Last Modified time: 2021-08-16 15:06:44
  */
 import Iconfont from "../iconfont";
 import { defineComponent, genComponentName } from "../modules/component";
@@ -19,6 +19,10 @@ export default defineComponent({
       type: Boolean,
       default: false,
     },
+    disableClick: {
+      type: Boolean,
+      default: false,
+    },
   },
   model: {
     prop: "checked",
@@ -26,7 +30,7 @@ export default defineComponent({
   },
   methods: {
     handleClick() {
-      if (!this.disabled) {
+      if (!this.disabled && !this.disableClick) {
         this.$emit("change", !this.checked);
       }
     },
